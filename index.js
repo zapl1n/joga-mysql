367745100f28e1d5de4fe3bd8b00ecd5de4cd693
Toomas <toomas.heljula@khk.ee>
2022-05-11 18:30:12 +0300

    Added handlebars template engine

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+//application packages
 const express = require('express');
 const app = express();
 
 const path = require('path');
-
+//add template engine
+const hbs = require('express-handlebars');
+//setup template engine directory and files extensions
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','hbs');
+app.engine('hbs',hbs.engine({
+    extname:'hbs',
+    defaultLayout:'main',
+    layoutsDir:__dirname+'/views/layouts/',
+}))
 const mysql = require('mysql');
 
 const bodyParser = require('body-parser');
