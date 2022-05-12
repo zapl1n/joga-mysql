20b3ea231876644752c493de22a71825dd60594c
Toomas <toomas.heljula@khk.ee>
2022-05-12 14:31:14 +0300

    displayed all articles

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine','hbs');
 app.engine('hbs',hbs.engine({
     extname:'hbs',
     defaultLayout:'main',
-    layoutsDir:__dirname+'/views/layouts/',
+    layoutsDir:__dirname + '/views/layouts/',
 }))
 //setup static public directory
 app.use(express.static('public'));
@@ -33,7 +33,20 @@ con.connect(function (err){
     if(err) throw err;
     console.log("Connected to joga_mysql db");
 })
+//show all articles
+app.get('/',(req,res)=>{
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query,(err, result)=>{
+        if(err) throw err;
+        articles = result
+        res.render('index',{
+            articles: articles
+        })
 
+    })
+
+});
 app.listen(3000,()=>{
-    console.log('App is started at http://localhost:3000');
+    console.log('App is started at http://localhost:3000')
 });
