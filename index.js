0b45107c324f7f2b0bf473d0d39838a70e78e50d
Toomas <toomas.heljula@khk.ee>
2022-05-12 17:15:09 +0300

    Displayed an article by slug

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 //application packages
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const path = require('path');
+const path = require('path')
+
 //add template engine
 const hbs = require('express-handlebars');
 //setup template engine directory and files extensions
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','hbs');
-app.engine('hbs',hbs.engine({
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
     extname:'hbs',
     defaultLayout:'main',
     layoutsDir:__dirname + '/views/layouts/',
@@ -33,20 +34,37 @@ con.connect(function (err){
     if(err) throw err;
     console.log("Connected to joga_mysql db");
 })
+
 //show all articles
-app.get('/',(req,res)=>{
+app.get('/',(req,res)=> {
     let query = "SELECT * FROM article";
     let articles = []
-    con.query(query,(err, result)=>{
-        if(err) throw err;
+    con.query(query, (err, result) => {
+        if (err) throw err;
         articles = result
-        res.render('index',{
+        res.render('index', {
             articles: articles
         })
 
     })
-
 });
+
+//show article by this slug
+    app.get('/article/:slug', (req,res)=>{
+        let query = `SELECT * FROM article WHERE slug ="${req.params.slug}"`
+        let article
+        con.query(query, (err, result)=>{
+            if(err) throw err;
+            article = result
+            console.log(article)
+            res.render('article',{
+                article:article
+
+        })
+    });
+});
+
+
 app.listen(3000,()=>{
     console.log('App is started at http://localhost:3000')
 });
