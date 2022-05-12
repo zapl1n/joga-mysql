745ed8cac372a7de845aa9e06fa9063ea033010d
Toomas <toomas.heljula@khk.ee>
2022-05-12 17:24:11 +0300

    Added author name into single article

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-//application packages
+// application packages
 const express = require('express')
 const app = express()
 
 const path = require('path')
-
-//add template engine
+// add template engine
 const hbs = require('express-handlebars');
-//setup template engine directory and files extensions
+// setup template engine directory and files extensions
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
-    extname:'hbs',
-    defaultLayout:'main',
-    layoutsDir:__dirname + '/views/layouts/',
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
 }))
-//setup static public directory
+
+// setup static public directory
 app.use(express.static('public'));
 
-const mysql = require('mysql');
+const mysql = require('mysql')
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:true}))
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
 
-
+// create database connection
 var con = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"qwerty",
-    database:"joga_mysql"
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "joga_mysql"
 })
 
-con.connect(function (err){
-    if(err) throw err;
+con.connect(function(err) {
+    if (err) throw err;
     console.log("Connected to joga_mysql db");
 })
 
-//show all articles
-app.get('/',(req,res)=> {
+// show all articles - index page
+app.get('/', (req, res) => {
     let query = "SELECT * FROM article";
     let articles = []
     con.query(query, (err, result) => {
@@ -45,26 +45,24 @@ app.get('/',(req,res)=> {
         res.render('index', {
             articles: articles
         })
-
     })
 });
 
 //show article by this slug
-    app.get('/article/:slug', (req,res)=>{
-        let query = `SELECT * FROM article WHERE slug ="${req.params.slug}"`
-        let article
-        con.query(query, (err, result)=>{
-            if(err) throw err;
-            article = result
-            console.log(article)
-            res.render('article',{
-                article:article
-
+app.get('/article/:slug', (req, res) => {
+    // let query = `SELECT * FROM article where slug = "${req.params.slug}"`
+    let query = `select article.id, article.name, article.slug, article.image, article.body, article.published, author.name as author from article JOIN author ON article.author_id = author.id where slug = "${req.params.slug}";`
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err
+        article = result
+        res.render('article', {
+            article: article
         })
-    });
+    })
 });
 
-
-app.listen(3000,()=>{
-    console.log('App is started at http://localhost:3000')
+// app start point
+app.listen(3000, () => {
+    console.log('App is started at http://localhost:3000');
 });
